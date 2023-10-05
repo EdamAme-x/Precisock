2dd3beb5d53d843232e695d02132bcdfa0770565
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-05 09:44:25 +0900

    Update View-LICENSE.zig

--- View-LICENSE.zig
+++ View-LICENSE.zig
@@ -6,4 +6,6 @@ const std = @import("std");
 
 pub fn main() !void {
     try std.io.getStdOut().writer().print(":: LICENSE ::\n");
+    // Repo: https://github.com/EdamAme-x/Precisock
+    try std.io.getStdOut().writer().print(":: Repo : https://github.com/EdamAme-x/Precisock ::\n");
 }
