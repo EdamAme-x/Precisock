855c2fb689879b244948315c3bfca9da00473a3a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-06 21:08:38 +0900

    del

--- sandbox/src/about.zig
+++
@@ -1,7 +0,0 @@
-pub fn Page(ctx) !void {
-    result = h.html(
-        {},
-        h.head({ lang: "en" }, h.title({}, "Precisock About Page")),
-        h.body({}, h.h1({}, "Precisock is a web framework for Zig!"))
-    );
-}
--- sandbox/src/api/manga.zig
+++
@@ -1,7 +0,0 @@
-pub fn Api(ctx) {
-    const manga_name = "Naruto";
-    
-    result = {
-        name: manga_name
-    }
-}
